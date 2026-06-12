0da161f3d1765f9bbbe357568dfb36376215a1a9
debshila <debshila@gmail.com>
2026-06-12 14:27:38 -0500

    Updated to include dplyr specification

--- R/get_survey_data.R
+++ R/get_survey_data.R
@@ -9,7 +9,8 @@
 #'
 #' @examples
 #' get_survey_data(selected_kinetic_survey_kwd = "Kinetic-Demographic")
-get_survey_data <- function(selected_kinetic_survey_kwd = "Kinetic-Demographic"){
+get_survey_data <- function(selected_kinetic_survey_kwd = "Kinetic-Demographic",
+                            file_location = "./"){
     require(qualtRics)
     # Get all surveys
     surveys <- all_surveys()
@@ -21,7 +22,7 @@ get_survey_data <- function(selected_kinetic_survey_kwd = "Kinetic-Demographic")
     # Select a survey
     survey_id <- kinetic_surveys %>%
         filter(grepl(selected_kinetic_survey_kwd, name)) %>%
-        select(id) %>%
+        dplyr::select(id) %>%
         unlist(use.names = FALSE)
 
     print(paste("The survey ID for", selected_kinetic_survey_kwd, "is", survey_id,
@@ -30,7 +31,14 @@ get_survey_data <- function(selected_kinetic_survey_kwd = "Kinetic-Demographic")
     # Get survey data
     survey_data <- fetch_survey(surveyID = survey_id, verbose = FALSE) %>%
         mutate(survey_id = survey_id)
+    # Extract questions
+    questions <- extract_colmap(survey_data)
 
+    question_csv_fname <- paste0(file_location, survey_id,
+                                 "_questions_", Sys.Date(), ".csv")
+    # Export questions as a csv
+    questions %>%
+        write_csv(question_csv_fname)
     # setClass("Qualtrics_info",
     #          representation = (surveys = "dataframe", survey_id = "character", survey_data = "dataframe"))
     return(survey_data)
